e178bce9deca7ed78d8c330ac9dc6483d896ab5c
tbrittain2 <tbrittain2@ucmerced.edu>
2021-12-07 14:57:25 -0800

    day 3 pt2 completed

--- day_3/part_2.py
+++ day_3/part_2.py
@@ -17,11 +17,34 @@ def filter_by_bit(bit, index, llist):
             new_list.append(seq)
     return new_list
 
-def bit_criteria(sequence):
+def bit_criteria(sequence,greatest):
+    print(sequence)
+    for bit_index in range(len(sequence[0])):
+        sig_count = 0
+        for seq in sequence:
+            if seq[bit_index] == '1':
+                sig_count += 1
+            else:
+                sig_count -= 1
+        print(f'sig count: {sig_count}')
+
+        if greatest:
+            if sig_count >= 0:
+                sequence = filter_by_bit('1',bit_index,sequence)
+            else:
+                sequence = filter_by_bit('0',bit_index,sequence)
+        else:
+            if sig_count >= 0:
+                print('FUCK')
+                sequence = filter_by_bit('0',bit_index,sequence)
+            else:
+                sequence = filter_by_bit('1',bit_index,sequence)
+        if len(sequence) == 1:
+            return sequence[0]
+    return sequence[0]
 
 
 sequences = []
-ones = 0
 
 with open('input.txt') as f:
     line = f.readline()
@@ -29,19 +52,10 @@ with open('input.txt') as f:
     while(line != ''):
         sequences.append(line.strip())
         line = f.readline()
+    oxygen = int(bit_criteria(sequences,True),2)
+    co2 = int(bit_criteria(sequences,False),2)
     
-    for bit_index in range(len(sequences[0])):
-        for seq in sequences:
-            if seq[bit_index] == '1':
-                ones += 1
-        if ones >= len(sequences)/2:
-            print('1')
-            sequences = filter_by_bit('1',bit_index,sequences)
-        else:
-            print('0')
-            sequences = filter_by_bit('0',bit_index,sequences)
-        ones = 0
-print(sequences)
-
-# print(sequences)
+print(f'Oxygen: {oxygen}')
+print(f'CO2: {co2}')
+print(f'Product: {oxygen * co2}')
 
