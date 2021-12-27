f4c039cce6b47f02a971ac595539416ea6ab4c6f
tylerBrittain42 <tbrittain100@gmail.com>
2021-12-27 00:04:38 -0800

    separated parts 1 and 2 for day 4

--- day_4/part_2.py
+++ day_4/part_2.py
@@ -3,6 +3,7 @@ class gameBoard:
     def __init__(self):
         self.rows = []
         self.bool_rows = []
+        self.score = -1
     
     def add_row(self, new_row):
         self.rows.append(new_row)
@@ -43,20 +44,13 @@ class gameBoard:
         # no win
         return False
 
-    def get_score(self,ans):
+    def calc_score(self,ans):
         score = 0
         for i, row in enumerate(self.bool_rows):
             for j, ele in enumerate(row):
                 if ele == False:
                     score += int(self.rows[i][j])
-
         return score * int(ans)
-
-
-
-
-
-
 
 def main():
 
@@ -90,13 +84,22 @@ def main():
         boards.append(new_board)
 
     # Checking the boards for a bingo
+    last_win = None
+    last_ans = None
+    has_won = []
     for ans in winning_sequence:
         for i, board in enumerate(boards):
             board.add_ans(ans.rstrip('\n'))
             if board.isWin():
-                print(f'Board {i} WON')
-                print(f'{board.get_score(ans)}')
-                return
+                if i not in has_won:
+                    has_won.append(i)
+                    last_win = i
+                    last_ans = ans
+                    board.score = board.calc_score(ans)
+                    print(f'Board {i} WON')
+                    print(f'{board.score}')
+                    
+    print(f'The last winner is {last_win} with a score of {boards[last_win].score}')
     
 
         
